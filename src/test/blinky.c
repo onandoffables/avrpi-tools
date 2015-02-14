27400e254f6bc392877d4d576bc2f99326e2fed2
onandoffables <on@onandoffables.com>
2015-02-14 20:39:58 +0100

    boards.txt + make blinky.hex more generic

--- src/test/blinky.c
+++ src/test/blinky.c
@@ -1,25 +1,33 @@
 // compile and upload with:
-// make flash
+// BRD=1 make flash
+// or
+// BRD=2 make flash
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED0    (1 << 0)
-#define LED1    (1 << 1)
-#define LED2    (1 << 5)
-#define LED3    (1 << 7)
-#define LEDMASK (LED0 | LED1 | LED2)
-//#define LEDMASK (LED3)
+// ATmega328p
+#if BRD == 1
+	#define LED    (1 << 5)
+	#define DIR    DDRB
+	#define PRT    PORTB
+#endif
+// ATmega32u4
+#if BRD == 2
+	#define LED    (1 << 7)
+	#define DIR    DDRC
+	#define PRT    PORTC
+#endif
 
 int main(void)
 {
-	DDRB |= LED2; // LED2 pin is output
-	PORTB &= ~(LED2 & LEDMASK); // LED2 is off
+	DIR |= LED; // LED pin is output
+	PRT &= ~(LED); // LED is off
 
 	for (;;) {
-		PORTB ^= (LED2 & LEDMASK); // invert LED2 pin
-		_delay_ms(50);
-		PORTB ^= (LED2 & LEDMASK); // invert LED2 pin
-		_delay_ms(950);
+		PRT ^= (LED); // invert LED pin
+		_delay_ms(500);
+		PRT ^= (LED); // invert LED pin
+		_delay_ms(500);
 	}
 
 	return 0;
