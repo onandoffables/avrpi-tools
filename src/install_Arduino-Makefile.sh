b8631568ebb1a2b75f32ea4830850b724d856b3c
onandoffables <on@onandoffables.com>
2015-02-14 21:54:34 +0100

    add Arduino-Makefile

---
+++ src/install_Arduino-Makefile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -d Arduino-Makefile ]; then
+	echo "Directory exists: updating"
+	cd Arduino-Makefile
+	git checkout avrm
+	git pull
+	cd ..
+else
+	echo "First time git clone: installing"
+	git clone https://github.com/onandoffables/Arduino-Makefile
+	cd Arduino-Makefile
+	git checkout avrm
+	cd ..
+fi
+
+
