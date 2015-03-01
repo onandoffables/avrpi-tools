d297cb96a717e0a20ddb1c649e3b485cebc42844
onandoffables <on@onandoffables.com>
2015-03-01 16:01:37 +0100

    remove libelf1 dep

--- install/avrdude-bin/install_avrdude_bin.sh
+++ install/avrdude-bin/install_avrdude_bin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # dependencies
-sudo apt-get install -y libftdi1 libelf1
+sudo apt-get install -y libftdi1
 
 # install deb
 sudo dpkg -i avrdude_6.1-2_armhf.deb
 
-# fix error by installing libftdi1 libelf1
+# fix error by installing libftdi1
 #sudo apt-get install -f
 
 # so we don't need sudo
