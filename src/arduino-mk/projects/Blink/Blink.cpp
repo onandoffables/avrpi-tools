7ec645299b300cdcb32f992303966783d0274257
onandoffables <on@onandoffables.com>
2015-04-05 22:26:22 +0200

    blinky pin

--- src/arduino-mk/projects/Blink/Blink.cpp
+++ src/arduino-mk/projects/Blink/Blink.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 
+int led = 13;
+
 void setup() {
-  pinMode(13, OUTPUT);      // pin 13 is an output.
+  pinMode(led, OUTPUT);      // ledpin is an output.
 }
 
 void loop() {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+  digitalWrite(led, HIGH);   // set the LED on
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // set the LED off
+  delay(1000);               // wait for a second
 }
